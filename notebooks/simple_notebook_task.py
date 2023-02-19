fe9db2b0d6047dcff3e3eaf77c71b71d091b1e2c
robertyousif1@gmail.com <robertyousif1@gmail.com>
2023-02-19 18:01:19 +0000

    Add Create Table statement

--- notebooks/simple_notebook_task.py
+++ notebooks/simple_notebook_task.py
@@ -1,9 +1,20 @@
 # Databricks notebook source
-print("Hej")
-from pyspark import spark 
+# MAGIC %md
+# MAGIC # Simple Notebook Task
+# MAGIC 
+# MAGIC This notebook is triggered by the dbx_cli_tutorial_workflow task called simple_notebook
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+spark = SparkSession.getActiveSession() # Setup spark for local testing, not needed when ran as notebook
 
 # COMMAND ----------
 
 df_gold = spark.sql(
-    "SELECT pickup_zip, SUM(trip_distance) as sum_trip_distance, SUM(fare_amount) as sum_fare_amount, avg(dollar_per_mile) as sum_dollar_per_mileduration_seconds FROM pax_demo.nyctaxi.silver_nytaxi GROUP BY pickup_zip ORDER BY sum_fare_amount DESC"
+    "SELECT pickup_zip, SUM(trip_distance) as sum_trip_distance, SUM(fare_amount) as sum_fare_amount, avg(duration_seconds) as avg_duration_seconds FROM pax_demo.nyctaxi.silver_nytaxi GROUP BY pickup_zip ORDER BY sum_fare_amount DESC"
 )
+
+# COMMAND ----------
+
+df_gold.write.mode("OVERWRITE").saveAsTable("pax_demo.nyctaxi.gold_nytaxi")
